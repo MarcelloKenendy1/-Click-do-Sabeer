113ff23873a16e50c3360b137e151364ec49954c
MarcelloKennedy1 <kennedymarcello75@gmail.com>
2025-08-21 17:21:57 -0300

    21/08 Marcello

---
+++ Assets/SCRIPTS/Menu.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+  
+    void Start()
+    {
+       
+    }
+
+    public void Jogar()
+    {
+       
+        Invoke("SelecionaPersonagens", 1f); //Chama a função SelecionaPersonagens após 1 segundo
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void TelaInicial()
+    {
+        SceneManager.LoadScene("TelaInicial");
+    }
+
+    public void Fase1()
+    {
+        SceneManager.LoadScene("Fase1");
+    }
+
+    public void portugues()
+    {
+        SceneManager.LoadScene("portugues");
+    }
+
+    public void matematica()
+    {
+        SceneManager.LoadScene("matematica");
+    }
+    public void ciencias()
+    {
+        SceneManager.LoadScene("ciencias");
+    }
+    public void geografia()
+    {
+        SceneManager.LoadScene("geografia");
+    }
+}
+       
