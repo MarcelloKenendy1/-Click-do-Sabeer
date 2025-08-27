9fc596320fe9f69a150393cd4fb115a9b7edd615
MarcelloKennedy1 <kennedymarcello75@gmail.com>
2025-08-27 16:04:18 -0300

    27/01
    viiiiiiiiiiiiiiiiiitu

--- Assets/SCRIPTS/Seconds.cs
+++ Assets/SCRIPTS/Seconds.cs
@@ -1,16 +1,80 @@
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEditor;
 
-public class Seconds : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static GameManager Instance;
+    public int score = 0;
+    public int record = 0;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+    public float gameDuration = 60f;
+    private float timeLeft;
+    private int comboCount = 0;
+    private bool comboActive = false;
+
+
+    void Awake() => Instance = this;
+
     void Start()
     {
-        
+        timeLeft = gameDuration;
+        record = PlayerPrefs.GetInt("Record", 0);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        timerText.text = Mathf.Ceil(timeLeft).ToString();
+
+        if (timeLeft <= 0)
+        {
+            EndGame();
+        }
     }
+
+    public void AddScore(float reactionTime = 0.5f)
+    {
+        int points = reactionTime < 0.3f ? 2 : 1;
+        score += comboActive ? points * 2 : points;
+        scoreText.text = score.ToString();
+
+        if (reactionTime < 0.3f)
+        {
+            comboCount++;
+            if (comboCount >= 5)
+                StartCoroutine(ActivateCombo());
+        }
+        else
+        {
+            comboCount = 0;
+        }
+    }
+
+    IEnumerator ActivateCombo()
+    {
+        comboActive = true;
+        yield return new WaitForSeconds(5f);
+        comboActive = false;
+        comboCount = 0;
+    }
+
+    public void EndGame()
+    {
+        if (score > record)
+        {
+            PlayerPrefs.SetInt("Record", score);
+        }
+        SceneManager.LoadScene("Result");
+    }
+
+    public void TriggerGameOver()
+    {
+        SceneManager.LoadScene("Result");
+    }
+
 }
